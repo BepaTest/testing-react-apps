9bb5c79bc089a0470e17d95fe7e62526ff6133fe
BepaTest <v.testard@dixeed.com>
2022-02-22 06:12:26 +0100

    first part finished

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -5,25 +5,55 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from '../../components/counter'
 
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
+  const div = document.createElement('div')
   //
   // 🐨 append the div to document.body (💰 document.body.append)
+  document.body.append(div)
   //
   // 🐨 use ReactDOM.render to render the <Counter /> to the div
+  ReactDOM.render(<Counter />, div)
   // 🐨 get a reference to the increment and decrement buttons:
   //   💰 div.querySelectorAll('button')
+  const button = div.querySelectorAll('button')
+  console.log(document.body.innerHTML)
+  const [decrement, increment] = div.querySelectorAll('button')
+
   // 🐨 get a reference to the message div:
   //   💰 div.firstChild.querySelector('div')
+  const messageDiv = div.firstChild.querySelector('div')
   //
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(messageDiv.textContent).toBe('Current count: 0')
   // 🐨 click the increment button (💰 increment.click())
   // 🐨 assert the message.textContent
+  increment.click()
+  console.log(document.body.innerHTML)
+  expect(messageDiv.textContent).toBe('Current count: 1')
   // 🐨 click the decrement button (💰 decrement.click())
+  decrement.click()
+  // console.log(document.body.innerHTML)
   // 🐨 assert the message.textContent
-  //
+  expect(messageDiv.textContent).toBe('Current count: 0')
+
+  const incrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0, //setting left button click
+  })
+
+  increment.dispatchEvent(incrementClickEvent)
+  console.log(document.body.innerHTML)
+  expect(messageDiv.textContent).toBe('Current count: 1')
+
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
   // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+  // div.remove()
 })
 
 /* eslint no-unused-vars:0 */
