51a7348959ee8ade59a3c6994db328e1aa668d62
BepaTest <v.testard@dixeed.com>
2022-02-27 11:47:17 +0100

    context and custom render method extra1

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -24,4 +24,22 @@ test('renders with the light styles for the light theme', () => {
   // ThemeProvider
 })
 
+test('renders with the dark styles for the dark theme', () => {
+  // 🐨 uncomment all of this code and your test will be busted on the next line:
+
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  )
+
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
+
+  // 🐨 update the `render` call above to use the wrapper option using the
+  // ThemeProvider
+})
+
 /* eslint no-unused-vars:0 */
