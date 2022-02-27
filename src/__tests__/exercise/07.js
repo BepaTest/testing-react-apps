998c22808a245c8afda9c5fb4f13abd86f60e9e0
BepaTest <v.testard@dixeed.com>
2022-02-27 12:04:52 +0100

    context and custom render method extra3

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,23 +2,23 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen} from 'test/test-utils'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-import {render as rtlRender} from '@testing-library/react'
-// "rtl" is short for "react testing library" not "right-to-left" 😅
-function myRender(ui, {theme = 'light', ...options} = {}) {
-  const Wrapper = ({children}) => (
-    <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
-  )
-  return render(ui, {wrapper: Wrapper, ...options})
-}
+// import {render as rtlRender} from '@testing-library/react'
+// // "rtl" is short for "react testing library" not "right-to-left" 😅
+// function myRender(ui, {theme = 'light', ...options} = {}) {
+//   const Wrapper = ({children}) => (
+//     <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+//   )
+//   return render(ui, {wrapper: Wrapper, ...options})
+// }
 
 test('renders with the light styles for the light theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
 
-  myRender(<EasyButton>Easy</EasyButton>, {theme: 'light'})
+  render(<EasyButton>Easy</EasyButton>, {theme: 'light'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -32,7 +32,7 @@ test('renders with the light styles for the light theme', () => {
 test('renders with the dark styles for the dark theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
 
-  myRender(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
