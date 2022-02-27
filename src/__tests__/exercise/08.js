9954934d343d5b50af1b1c9d1f430a845b5ee97a
BepaTest <v.testard@dixeed.com>
2022-02-27 14:28:26 +0100

    testing custom hooks

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
+import Counter from 'components/counter'
 
 // 🐨 create a simple function component that uses the useCounter hook
 // and then exposes some UI that our test can interact with to test the
@@ -12,11 +13,29 @@ import useCounter from '../../components/use-counter'
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
 
+function Couner() {
+  const {count, increment, decrement} = useCounter()
+  return (
+    <>
+      <div>Current count: {count}</div>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </>
+  )
+}
+
 test('exposes the count and increment/decrement functions', () => {
   // 🐨 render the component
+  render(<Counter />)
   // 🐨 get the elements you need using screen
+  const message = screen.getByText(/current count/i)
+  expect(message).toHaveTextContent('Current count: 0')
   // 🐨 assert on the initial state of the hook
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  userEvent.click(screen.getByRole('button', {name: /decrement/i}))
+  expect(message).toHaveTextContent('Current count: -1')
+  userEvent.click(screen.getByRole('button', {name: /increment/i}))
+  expect(message).toHaveTextContent('Current count: 0')
 })
 
 /* eslint no-unused-vars:0 */
